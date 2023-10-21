22f97efbd9fcb3b60ac861efa6610e25bf62f7d9
Aayush Srivastav <139948534+AayushSrivastav29@users.noreply.github.com>
2023-10-21 18:59:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheckbox = document.querySelector(".keys-checkbox input");
+
+let allKeys = [];
+let audio = new Audio(`tunes/a.wav`);
+let isRecording = false;
+const recordedTunes = [];
+
+const playTune = (key) => {
+  audio.src = `tunes/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
+
+  if (isRecording) {
+    recordedTunes.push(key);
+  }
+};
+
+pianoKeys.forEach((key) => {
+  allKeys.push(key.dataset.key);
+  key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+};
+
+const showHideKeys = () => {
+  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
+
+const pressedKey = (e) => {
+  if (allKeys.includes(e.key)) playTune(e.key);
+};
+
+keysCheckbox.addEventListener("click", showHideKeys);
+volumeSlider.addEventListener("input", handleVolume);
+document.addEventListener("keydown", pressedKey);
+
+// Event listener for the record button
+document.getElementById("record-button").addEventListener("click", () => {
+  isRecording = true;
+  recordedTunes.length = 0; // Clear any previous recordings
+  displayMessage("Recording started...");
+});
+
+// Event listener for the stop recording button
+document.getElementById("stop-record-button").addEventListener("click", () => {
+  isRecording = false;
+  displayMessage("Recording stopped.");
+});
+
+// Event listener for the playback button
+document.getElementById("playback-button").addEventListener("click", () => {
+  if (recordedTunes.length > 0) {
+    playRecordedTunes(recordedTunes, () => {
+      // Playback completed; trigger confetti animation
+      triggerConfetti();
+    });
+  }
+});
+
+function playRecordedTunes(tunes) {
+  let index = 0;
+  const playbackInterval = setInterval(() => {
+    if (index < tunes.length) {
+      playTune(tunes[index]);
+      index++;
+    } else {
+      clearInterval(playbackInterval);
+    }
+  }, 600);
+}
+
+// Function to display messages
+function displayMessage(message) {
+  const messageBox = document.querySelector(".message-box");
+  messageBox.textContent = message;
+  messageBox.style.display = "block";
+
+  // Automatically hide the message after a few seconds
+  setTimeout(() => {
+    messageBox.style.display = "none";
+  }, 3000); // Adjust the time as needed (3000ms = 3 seconds)
+}
+
+// Function to trigger the confetti animation
+function triggerConfetti() {
+  startConfetti(); // Call the startConfetti function from the confetti library
+
+  // Optionally, you can add a timer to stop the confetti after a certain duration
+  setTimeout(stopConfetti, 5000); // Stop confetti after 5 seconds (adjust the duration as needed)
+}
